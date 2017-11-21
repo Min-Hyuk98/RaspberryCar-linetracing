3be60b6724b0ca418727cb74a17e833b834dee47
Min-Hyuk98 <wkdalsgur85@naver.com>
2017-11-21 10:51:33 +0000

    linetracing + tracking

---
+++ TurningCar.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+from ForwardBackwardBase import *
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+# 구동체의 턴 관련 함수가 포함된 모듈
+
+# 오른쪽으로 스윙턴 하는 함수
+def rightSwingTurn(speed, running_time):
+    leftMotor("forward")
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    GPIO.output(MotorRight_PWM, GPIO.LOW)
+
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(0)
+
+    sleep(running_time)
+
+
+# 왼쪽으로 스윙턴 하는 함수
+def leftSwingTurn(speed, running_time):
+    rightMotor("forward")
+    GPIO.output(MotorLeft_PWM, GPIO.LOW)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+
+    LeftPwm.ChangeDutyCycle(0)
+    RightPwm.ChangeDutyCycle(speed)
+
+    sleep(running_time)
+
+
+# 오른쪽으로 포인트턴 하는 함수
+def rightPointTurn(speed, running_time):
+    leftMotor("forward")
+    rightMotor("backward")
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(speed)
+
+    sleep(running_time)
+
+
+# 왼쪽으로 포인트턴 하는 함수
+def leftPointTurn(speed, running_time):
+    leftMotor("backward")
+    rightMotor("forward")
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(speed)
+
+    sleep(running_time)
+
+
+# 모듈 테스트
+if __name__ == "__main__":
+    # set up GPIO mode as BOARD
+    GPIO.setmode(GPIO.BOARD)
+    #baseSetup()
+    try:
+        LeftPwm.start(0)
+        RightPwm.start(0)
+
+        rightSwingTurn(45, 2)
+        sleep(2)
+        leftSwingTurn(45, 2)
+        sleep(2)
+        rightPointTurn(45, 2)
+        sleep(2)
+        leftPointTurn(45, 2)
+
+    except KeyboardInterrupt:
+        stopCar()
---
+++ assignment4_Main.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+from ForwardBackwardBase import *
+from TurningCar import rightPointTurn, leftPointTurn
+from UltraSensor import getDistance
+from Tracking import track
+import RPi.GPIO as GPIO
+from time import sleep
+
+speed = 15
+
+
+def avoid():
+    try:
+        LeftPwm.ChangeDutyCycle(0)
+        RightPwm.ChangeDutyCycle(0)
+        sleep(1)
+        ##########################################
+        # 상황에 따라서 다르기 때문에 각자 조절해야함
+        rightPointTurn(25, 0.5)
+        sleep(1)
+        goForward(50, 1.4)
+        leftPointTurn(28, 0.65)
+        ##########################################
+        sleep(1.5)
+        goForwardAny(speed)
+    except KeyboardInterrupt:
+        stopCar()
+        GPIO.cleanup()
+
+
+def lineTrace():
+    # goForwardAny(speed)
+    # while True:
+    try:
+        led_list = track()
+
+        if led_list[0] and not (led_list[4]):
+            # print 'a'
+            LeftPwm.ChangeDutyCycle(speed + 70)
+            RightPwm.ChangeDutyCycle(speed)
+        if led_list[1] and not (led_list[3]):
+            # print 'b'
+            LeftPwm.ChangeDutyCycle(speed + 60)
+            RightPwm.ChangeDutyCycle(speed)
+        if led_list[3] and not (led_list[1]):
+            # print 'c'
+            LeftPwm.ChangeDutyCycle(speed)
+            RightPwm.ChangeDutyCycle(speed + 40)
+        if led_list[4] and not (led_list[0]):
+            # print 'd'
+            LeftPwm.ChangeDutyCycle(speed)
+            RightPwm.ChangeDutyCycle(speed + 60)
+
+    except KeyboardInterrupt:
+        stopCar()
+        GPIO.cleanup()
+
+
+def start():
+    try:
+        goForwardAny(speed)
+        while True:
+            a = getDistance()
+            if a > 22:
+                lineTrace()
+            else:
+                # print 'avoid'
+                avoid()
+
+    except KeyboardInterrupt:
+        stopCar()
+        GPIO.cleanup()
+
+
+def start2():
+    goForwardAny(speed)
+    while True:
+        lineTrace()
+
+
+if __name__ == "__main__":
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+
+    GPIO.setup(MotorLeft_A, GPIO.OUT)
+    GPIO.setup(MotorLeft_B, GPIO.OUT)
+    GPIO.setup(MotorLeft_PWM, GPIO.OUT)
+
+    GPIO.setup(MotorRight_A, GPIO.OUT)
+    GPIO.setup(MotorRight_B, GPIO.OUT)
+    GPIO.setup(MotorRight_PWM, GPIO.OUT)
+
+    # 과제 진행
+    try:
+        LeftPwm.start(0)
+        RightPwm.start(0)
+        goForward(speed, 0.3)
+        start()
+
+    except KeyboardInterrupt:
+        stopCar()
+        GPIO.cleanup()
